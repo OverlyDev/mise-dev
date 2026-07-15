8bce31a7862609ccc01e329fa1e0911e5ed4d5f2
OverlyDev <55297573+OverlyDev@users.noreply.github.com>
2026-07-14 23:54:57 -0400

    some updates

--- README.md
+++ README.md
@@ -10,6 +10,27 @@
     - We use `exec` to replace our current shell after things get added/updated
     - This removes the requirement to close/reopen the terminal to load the changes
 
+#### VSCode
+
+> This section is a work in progress!
+
+To setup VSCode with mise, add the below lines to your user configuration.
+
+Access via: Ctrl + Shift + P -> Open User Settings (JSON)
+```json
+{
+    "mise.configureExtensionsAutomatically": true,
+    "mise.configureExtensionsIncludeGlobalTools": true
+}
+```
+Reload via: Ctrl + Shift + P -> Reload Window
+
+The combo of the above config and `mise.toml` adding shims to `~/.bash_profile` *should* make extensions "just work".
+
+For extensions that still don't work, point them manually to the binary at:
+- `$MISE_DATA_DIR/shims/<binary name>`
+  - Note: Use the full/resolved path, no variables
+
 ### Uninstall
 
 1. Run uninstall script: `exec bash uninstall.sh`
--- uninstall.sh
+++ uninstall.sh
@@ -2,16 +2,35 @@
 
 set -euo pipefail
 
+# Helper function to remove mise-managed lines from specified file
+cleanup_mise_lines() {
+    local file="$1"
+    local backup="$1.bak-$(date +'%s')"
+
+    echo "Backing up $file to $backup"
+    cp "$file" "$backup"
+
+    echo "Removing mise-managed lines from $file:"
+    # Preview lines to be removed
+    sed -E -n '/# >>> mise:.*>>>/,/# <<< mise:.*<<</p' "$file"
+
+    # Actually delete lines
+    sed -E -i '/# >>> mise:.*>>>/,/# <<< mise:.*<<</d' "$file"
+}
+
 echo "Removing mise"
 mise implode || true
 
-echo "Making backup of ~/.bashrc to ~/.bashrc.bak"
-echo "Removing mise-managed lines from ~/.bashrc:"
-# Preview lines to be removed
-sed -E -n '/# >>> mise:.*>>>/,/# <<< mise:.*<<</p' ~/.bashrc
+# echo "Making backup of ~/.bashrc to ~/.bashrc.bak"
+# echo "Removing mise-managed lines from ~/.bashrc:"
+# # Preview lines to be removed
+# sed -E -n '/# >>> mise:.*>>>/,/# <<< mise:.*<<</p' ~/.bashrc
 
-# Actually delete lines
-sed -E -i'.bak' '/# >>> mise:.*>>>/,/# <<< mise:.*<<</d' ~/.bashrc
+# # Actually delete lines
+# sed -E -i'.bak' '/# >>> mise:.*>>>/,/# <<< mise:.*<<</d' ~/.bashrc
+
+cleanup_mise_lines "$HOME/.bashrc"
+cleanup_mise_lines "$HOME/.bash_profile"
 
 echo "Removing ~/.config/mise/"
 rm -rf ~/.config/mise
